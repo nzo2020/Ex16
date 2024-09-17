56103395fcf3d416237592675ff50be66617c9f5
nzo2020 <nz2020@bs.amalnet.k12.il>
2024-09-17 23:36:08 +0300

    Initial commit

---
+++ app/src/main/java/com/example/ex16/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.ex16;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnRed, btnBlue;
+    int blueScore = 0, redScore = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnRed = (Button) findViewById(R.id.btnRed);
+        btnBlue = (Button) findViewById(R.id.btnBlue);
+
+        btnBlue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                blueScore++;
+            }
+        });
+
+        btnRed.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                redScore += 2;
+                return true;
+            }
+        });
+    }
+
+    public void clickedResult(View view) {
+        Intent si = new Intent(this, MainActivity2.class);
+        si.putExtra("blueScore", blueScore);
+        si.putExtra("redScore", redScore);
+        startActivity(si);
+    }
+}
---
+++ app/src/main/java/com/example/ex16/MainActivity2.java
@@ -0,0 +1,39 @@
+package com.example.ex16;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        TextView blueScoreTextView = findViewById(R.id.bstv);
+        TextView redScoreTextView = findViewById(R.id.rstv);
+        TextView winnerTextView = findViewById(R.id.wtv);
+
+        Intent gi = getIntent();
+        int blueScore = gi.getIntExtra("blueScore", 0);
+        int redScore = gi.getIntExtra("redScore", 0);
+
+        blueScoreTextView.setText("Blue: " + blueScore);
+        redScoreTextView.setText("Red: " + redScore);
+
+        if (blueScore > redScore) {
+            winnerTextView.setText("Winner: Blue");
+        } else if (redScore > blueScore) {
+            winnerTextView.setText("Winner: Red");
+        } else {
+            winnerTextView.setText("It's a tie!");
+        }
+    }
+
+    public void clickedBack(View view) {
+        finish();
+    }
+}
